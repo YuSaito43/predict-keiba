07d346bd7269afe4281ffc42c5c981475cb548d2
YuSaito43 <84009270+YuSaito43@users.noreply.github.com>
2022-10-05 02:33:30 +0900

    Add files via upload

--- scraping_traing_data.py
+++ scraping_traing_data.py
@@ -12,7 +12,7 @@ years = ['2016', '2017', '2018', '2019', '2020', '2021']
 ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36 Edg/90.0.818.46'
 headers = {'User-Agent' : ua}
 for year in years:
-    url = f"https://keiba.yahoo.co.jp/schedule/list/{year}/?grade_one=1"
+    url = f"https://sports.yahoo.co.jp/keiba/schedule/yearly?year={year}&type=g1"
     column = ['着順', '枠番', '馬名', '父親', '母親', '性別', '年齢', '体重変化量', '騎手', '人気', '調教師', '種別', '回り方', '距離', '天気', '状態']
 
     text = requests.get(url, headers=headers)
@@ -29,17 +29,17 @@ for year in years:
     res_pd = 0
     flag = 0
     for con in urllist:
-        url = "https://keiba.yahoo.co.jp" + con
+        url = "https://sports.yahoo.co.jp" + con
         text = requests.get(url, headers=headers)
         text.encoding = text.apparent_encoding
         ssoup = bs(text.text, "lxml")
     
-        types = ssoup.find('p', id="raceTitMeta").text.split(' | ')[0].split(' ')[0].split('・')[0]    #種別
-        rotate = ssoup.find('p', id="raceTitMeta").text.split(' | ')[0].split(' ')[0].split('・')[1]   #回り方
-        length = ssoup.find('p', id="raceTitMeta").text.split(' | ')[0].split(' ')[-1]                 #距離
-        weather = ssoup.find('p', id="raceTitMeta").find_all('img')[0].get('alt')                      #天気
-        condition = ssoup.find('p', id="raceTitMeta").find_all('img')[1].get('alt')                    #状態
-        test = ssoup.find('table', id="raceScore").find_all('tr')
+        types = ssoup.find_all('section', id='racedetail')[0].find_all('span')[1].text.split('・')[0]    #種別
+        rotate = ssoup.find_all('section', id='racedetail')[0].find_all('span')[1].text.split('・')[1].split(' ')[0]   #回り方
+        length = ssoup.find_all('section', id='racedetail')[0].find_all('span')[1].text.split('・')[1].split(' ')[-1]                 #距離
+        weather = ssoup.find_all('section', id='racedetail')[0].find_all('span')[2].text.split('：')[-1]                      #天気
+        condition = ssoup.find_all('section', id='racedetail')[0].find_all('span')[3].text.split('：')[-1]                   #状態
+        test = ssoup.find('table', class_='hr-table').find_all('tr')
         alllist = []
         for i in range(1, len(test)):
             tlist = []
@@ -56,28 +56,24 @@ for year in years:
                 tlist.append(re.sub(r'[\n\t\s]', '', temp[0].text)) #着順
             tlist.append(re.sub(r'[\n\t\s]', '', temp[1].text))     #枠番
             tlist.append(re.sub(r'[\n\t\s]', '', temp[3].find('a').text))   #馬名
-            umaURL = 'https://keiba.yahoo.co.jp' + temp[3].find('a').get('href')
+            umaURL = 'https://sports.yahoo.co.jp/' + temp[3].find('a').get('href')
             uma_text = requests.get(umaURL, headers=headers)
             uma_text.encoding = uma_text.apparent_encoding
             uma_soup = bs(uma_text.text, "lxml")
-            if len(uma_soup.find_all('table', id='dirUmaBlood')) == 0:
+            if len(uma_soup.find_all('section', class_='hr-horseProfile')[0].find('div', class_='hr-horsePedigree__gen1st').find_all('div')) == 0:
                 tlist.append('-')
                 tlist.append('-')
             else:
-                parents_list = uma_soup.find_all('table', id='dirUmaBlood')[0].find_all('td')
-                parents = []
-                for pcon in parents_list:
-                    if pcon.get('rowspan') == '4':
-                        parents.append(pcon.text)
-                tlist.append(parents[0])   #父親
-                tlist.append(parents[1])   #母親
-            if re.sub(r'[\n\t\s]', '', temp[3].find('span').text).split('/')[0][0] =='せ':
-                tlist.append(re.sub(r'[\n\t\s]', '', temp[3].find('span').text).split('/')[0][:2])
-                tlist.append(re.sub(r'[\n\t\s]', '', temp[3].find('span').text).split('/')[0][2:])
+                parents_list = uma_soup.find_all('section', class_='hr-horseProfile')[0].find('div', class_='hr-horsePedigree__gen1st').find_all('div')
+                tlist.append(re.sub(r'[\n\t\s]', '', parents_list[0].text))   #父親
+                tlist.append(re.sub(r'[\n\t\s]', '', parents_list[1].text))   #母親
+            if re.sub(r'[\n\t\s]', '', temp[3].find('p').text).split('/')[0][0] =='せ':
+                tlist.append(re.sub(r'[\n\t\s]', '', temp[3].find('p').text).split('/')[0][:2])     #性別
+                tlist.append(re.sub(r'[\n\t\s]', '', temp[3].find('p').text).split('/')[0][2:])     #年齢
             else:
-                tlist.append(re.sub(r'[\n\t\s]', '', temp[3].find('span').text).split('/')[0][0])   #性別
-                tlist.append(re.sub(r'[\n\t\s]', '', temp[3].find('span').text).split('/')[0][1:])  #年齢
-            tnum = re.sub(r'[\n\t\s]', '', temp[3].find('span').text).split('/')[1]
+                tlist.append(re.sub(r'[\n\t\s]', '', temp[3].find('p').text).split('/')[0][0])   #性別
+                tlist.append(re.sub(r'[\n\t\s]', '', temp[3].find('p').text).split('/')[0][1:])  #年齢
+            tnum = re.sub(r'[\n\t\s]', '', temp[3].find('p').text).split('/')[1]
             tlist.append(tnum[tnum.find('(')+1:tnum.find(')')].strip('+'))                          #体重変化量
             tlist.append(re.sub(r'[\n\t\s]', '', temp[6].find('a').text))                           #騎手
             tlist.append(re.findall(r'\d+', re.sub(r'[\n\t\s]', '', temp[7].text))[0])              #人気
